6aec0563d71a053804c6ff6cc085d94304e0800f
Lennart K <lennart.karsten@haw-hamburg.de>
2016-03-08 15:47:56 +0100

    more gradle to maven changes

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM artifactory.mars.haw-hamburg.de:5000/niaquinto/gradle
 
 COPY [".", "/gisimport"]
 WORKDIR /gisimport
-RUN gradle build
-RUN cp build/libs/mars-gis-import-0.0.1-SNAPSHOT.jar /app.jar
+RUN mvn package
+ADD target/gis-import-0.0.1-SNAPSHOT.jar app.jar
 RUN rm -rf /gisimport
 
 EXPOSE 8080
--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-gradle build
+# This builds the gis-import and restarts the gisimport docker container inside the websuite.
+# It requires the websuite to be running and that both git repos are cloned to the same folder.
+
+mvn package
 docker-compose -f docker-compose_dev.yml build
-docker-compose -f docker-compose_dev.yml stop
-docker-compose -f docker-compose_dev.yml up
+cd ../marscloudinanutshell
+docker-compose -f new_import_websuite_development.yml stop gisimport
+docker-compose -f new_import_websuite_development.yml rm -f gisimport
+docker-compose -f new_import_websuite_development.yml up gisimport
