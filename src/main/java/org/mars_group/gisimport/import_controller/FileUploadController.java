7bc111e4f7eed5dc5bdce049dc5c8bffdbb683b4
Lennart K <lennart.karsten@haw-hamburg.de>
2016-05-04 13:52:06 +0200

    fix new upload structure

--- src/main/java/org/mars_group/gisimport/import_controller/FileUploadController.java
+++ src/main/java/org/mars_group/gisimport/import_controller/FileUploadController.java
@@ -5,6 +5,7 @@ import de.haw_hamburg.mars.mars_group.metadataclient.MetadataClient;
 import org.apache.commons.io.FileUtils;
 import org.mars_group.gisimport.exceptions.GisImportException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,20 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
 
 
 @RestController
 class FileUploadController {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     private static final String uploadDir = "upload-dir";
 
@@ -30,41 +40,28 @@ class FileUploadController {
     @Autowired
     RestTemplate restTemplate;
 
-    }
-
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/import")
-    public ResponseEntity<String> handleImport( @RequestParam String importId, @RequestParam String title, @RequestParam String filename) {
-        return restTemplate.execute("http://upload-service:3333/upload/" + importId, HttpMethod.GET, null,
-                new ResponseExtractor<ResponseEntity<String>>() {
-                    @Override
-                    public ResponseEntity<String> extractData(ClientHttpResponse response) throws IOException {
-                        try {
-                            saveFile(response.getBody(), uploadDir + File.separator + importId, filename);
-                            handleUpload(title, filename);
-                        } catch (GisImportException e) {
-                            e.printStackTrace();
-                        }
-                        return new ResponseEntity<>("saved", HttpStatus.CREATED);
+    public ResponseEntity<String> handleImport(@RequestParam String importId,
+                                               @RequestParam String title,
+                                               @RequestParam String filename) {
+
+        return restTemplate.execute("http://upload:3333/upload/" + importId, HttpMethod.GET, null,
+                response -> {
+                    try {
+                        String specificUploadDir = uploadDir + File.separator + importId;
+
+                        saveFile(response.getBody(), specificUploadDir, filename);
+                        handleUpload(title, filename, importId, specificUploadDir);
+
+                    } catch (GisImportException e) {
+                        e.printStackTrace();
                     }
+                    return new ResponseEntity<>("saved", HttpStatus.CREATED);
                 });
     }
 
-    private ResponseEntity<String> handleUpload(String title, String filename) {
-
-        if (!new File(uploadDir).exists()) {
-            if (!new File(uploadDir).mkdir()) {
-                return new ResponseEntity<>("Failed to create general upload dir!", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        String importId = UUID.randomUUID().toString();
-        String specificUploadDir = uploadDir + File.separator + importId;
-        if (!new File(specificUploadDir).exists()) {
-            if (!new File(specificUploadDir).mkdir()) {
-                return new ResponseEntity<>("Failed to create upload dir!", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+    private ResponseEntity<String> handleUpload(String title, String filename, String importId, String specificUploadDir) {
 
         MetadataClient metadataClient = MetadataClient.getInstance(new RestTemplate(), "http://metadata:4444");
         metadataClient.setState(importId, ImportState.PROCESSING);
@@ -89,8 +86,15 @@ class FileUploadController {
     }
 
     private void saveFile(InputStream file, String specificUploadDir, String filename) throws GisImportException {
-
         try {
+            if (!new File(uploadDir).exists()) {
+                assertTrue(new File(uploadDir).mkdir());
+            }
+
+            if (!new File(specificUploadDir).exists()) {
+                assertTrue(new File(specificUploadDir).mkdir());
+            }
+
             // save file
             File f = new File(specificUploadDir + File.separator + filename);
             BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(f));
