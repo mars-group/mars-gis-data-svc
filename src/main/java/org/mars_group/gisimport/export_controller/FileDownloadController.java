34b87357a69eff9c3f6c4653e59c2e6f5621fc26
Lennart K <lennart.karsten@haw-hamburg.de>
2016-05-09 13:48:39 +0200

    cleanup Filedownload

--- src/main/java/org/mars_group/gisimport/export_controller/FileDownloadController.java
+++ src/main/java/org/mars_group/gisimport/export_controller/FileDownloadController.java
@@ -1,75 +1,43 @@
 package org.mars_group.gisimport.export_controller;
 
-import org.mars_group.gisimport.exceptions.GisImportException;
-import org.mars_group.gisimport.util.UploadType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.core.UriBuilder;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 
 @RestController
 public class FileDownloadController {
 
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET, value = "/download/shp")
-    public ResponseEntity<String> downloadShp(
-            @RequestParam String layername
-    ) throws IOException, GisImportException {
+    @RequestMapping(method = RequestMethod.GET, value = "/download/vector")
+    public ResponseEntity<String> downloadShp(@RequestParam String importId,
+                                              @RequestParam String layername) {
 
-        String url;
-        try {
-            url = buildString(UploadType.SHP, layername);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("error while trying to download: " + layername, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        String url = UriBuilder.fromUri("/geoserver")
+                .path("wfs")
+                .queryParam("request", "GetFeature")
+                .queryParam("version", "2.0.0")
+                .queryParam("typeName", importId + ":" + layername)
+                .queryParam("outputFormat", "shape-zip")
+                .build().toString();
         return new ResponseEntity<>(url, HttpStatus.OK);
     }
 
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET, value = "/download/geotiff")
-    public ResponseEntity<String> downloadGeoTiff(
-            @RequestParam String layername
-    ) throws IOException, GisImportException {
+    @RequestMapping(method = RequestMethod.GET, value = "/download/raster")
+    public ResponseEntity<String> downloadGeoTiff(@RequestParam String importId,
+                                                  @RequestParam String layername) {
 
-        String url;
-        try {
-            url = buildString(UploadType.GEOTIFF, layername);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("error while trying to download: " + layername, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        String url = UriBuilder.fromUri("/geoserver")
+                .path("wcs")
+                .queryParam("request", "GetCoverage")
+                .queryParam("service", "WCS")
+                .queryParam("version", "2.0.1")
+                .queryParam("coverageId", importId + ":" + layername)
+                .queryParam("format", "ArcGrid")
+                .build().toString();
         return new ResponseEntity<>(url, HttpStatus.OK);
-    }
-
-    private String buildString(UploadType uploadType, String layername) throws URISyntaxException, MalformedURLException, GisImportException {
-        String uri;
-        if (uploadType.equals(UploadType.SHP)) {
-            uri = UriBuilder.fromUri("/geoserver")
-                    .path("wfs")
-                    .queryParam("request", "GetFeature")
-                    .queryParam("version", "2.0.0")
-                    .queryParam("typeName", layername)
-                    .queryParam("outputFormat", "shape-zip")
-                    .build().toString();
-        } else {
-            uri = UriBuilder.fromUri("/geoserver")
-                    .path("wcs")
-                    .queryParam("request", "GetCoverage")
-                    .queryParam("service", "WCS")
-                    .queryParam("version", "2.0.1")
-                    .queryParam("coverageId", layername)
-                    .queryParam("format", "geotiff")
-                    .build().toString();
-        }
-
-        return uri;
     }
 }
