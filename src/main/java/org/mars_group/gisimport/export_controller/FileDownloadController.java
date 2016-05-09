bd30428984e11d461439e3c1f07f0becaebac8b5
Lennart K <lennart.karsten@haw-hamburg.de>
2016-05-09 14:39:13 +0200

    comment REST points

--- src/main/java/org/mars_group/gisimport/export_controller/FileDownloadController.java
+++ src/main/java/org/mars_group/gisimport/export_controller/FileDownloadController.java
@@ -10,34 +10,46 @@ import javax.ws.rs.core.UriBuilder;
 @RestController
 public class FileDownloadController {
 
+    /**
+     * Download Vector files like shapefiles
+     * @param importId id created during import
+     * @param datasetName the name of the files inside the Zip file without the file extension.
+     * @return relative uri to the file
+     */
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, value = "/download/vector")
     public ResponseEntity<String> downloadShp(@RequestParam String importId,
-                                              @RequestParam String layername) {
+                                              @RequestParam String datasetName) {
 
-        String url = UriBuilder.fromUri("/geoserver")
+        String uri = UriBuilder.fromUri("/geoserver")
                 .path("wfs")
                 .queryParam("request", "GetFeature")
                 .queryParam("version", "2.0.0")
-                .queryParam("typeName", importId + ":" + layername)
+                .queryParam("typeName", importId + ":" + datasetName)
                 .queryParam("outputFormat", "shape-zip")
                 .build().toString();
-        return new ResponseEntity<>(url, HttpStatus.OK);
+        return new ResponseEntity<>(uri, HttpStatus.OK);
     }
 
+    /**
+     * Download raster files like AsciiGrid or GeoTiff
+     * @param importId id created during import
+     * @param title the title you specified during import
+     * @return relative uri to the file
+     */
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, value = "/download/raster")
     public ResponseEntity<String> downloadGeoTiff(@RequestParam String importId,
-                                                  @RequestParam String layername) {
+                                                  @RequestParam String title) {
 
-        String url = UriBuilder.fromUri("/geoserver")
+        String uri = UriBuilder.fromUri("/geoserver")
                 .path("wcs")
                 .queryParam("request", "GetCoverage")
                 .queryParam("service", "WCS")
                 .queryParam("version", "2.0.1")
-                .queryParam("coverageId", importId + ":" + layername)
+                .queryParam("coverageId", importId + ":" + title)
                 .queryParam("format", "ArcGrid")
                 .build().toString();
-        return new ResponseEntity<>(url, HttpStatus.OK);
+        return new ResponseEntity<>(uri, HttpStatus.OK);
     }
 }
