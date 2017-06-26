97760924dfcd346df1a9d0b56cab1efcd14921f5
Lennart Karsten <lennart.karsten@haw-hamburg.de>
2017-06-26 09:59:01 +0200

    cleanup

--- src/main/java/org/mars_group/gisimport/controller/GeoServerExport.java
+++ src/main/java/org/mars_group/gisimport/controller/GeoServerExport.java
@@ -3,7 +3,6 @@ package org.mars_group.gisimport.controller;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
 import it.geosolutions.geoserver.rest.decoder.RESTLayer;
 import org.apache.commons.io.FileUtils;
-import org.geotools.coverage.grid.GridCoordinates2D;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridCoverage2DReader;
@@ -13,8 +12,6 @@ import org.mars_group.core.Metadata;
 import org.mars_group.gisimport.exceptions.GisImportException;
 import org.mars_group.gisimport.util.GeoServer;
 import org.mars_group.metadataclient.MetadataClient;
-import org.opengis.coverage.grid.GridCoordinates;
-import org.opengis.coverage.grid.GridEnvelope;
 import org.opengis.parameter.GeneralParameterValue;
 import org.opengis.parameter.ParameterValue;
 import org.opengis.referencing.operation.TransformException;
@@ -34,8 +31,9 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.text.NumberFormat;
-import java.util.*;
-import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
 
 @Component
 class GeoServerExport {
@@ -90,15 +88,18 @@ class GeoServerExport {
     double getValue(String dataId, Point2D gps) throws IOException, GisImportException, TransformException {
         // ToDo: handle vector file.
 
-        File file = downloadFile(dataId);
+        File file = new File(dataId + ".tif");
+
+        if (!file.exists()) {
+            file = downloadFile(file, dataId);
+        }
 
         return readPixelValue(file, convertGpsToPixels(gps));
     }
 
-    private File downloadFile(String dataId) throws IOException, GisImportException {
+    private File downloadFile(File file, String dataId) throws IOException, GisImportException {
         URL url = new URL(geoServer.getBaseUrl() + "/" + getUriFromDataId(dataId));
 
-        File file = new File("tmpFile.tif");
         FileUtils.copyURLToFile(url, file);
 
         return file;
@@ -106,19 +107,18 @@ class GeoServerExport {
 
     // TODO: implement for real
     private Point convertGpsToPixels(Point2D gps) {
-        System.out.println("gps: " + gps);
+//        System.out.println("gps: " + gps);
 
         Random rnd = new Random();
 
         Point pixel = new Point(rnd.nextInt(100), rnd.nextInt(100));
 
-        System.out.println("Pixel: " + pixel);
+//        System.out.println("Pixel: " + pixel);
 
         return pixel;
     }
 
     private double readPixelValue(File file, Point pixels) throws IOException, TransformException {
-        long startTime = System.nanoTime();
         ParameterValue<OverviewPolicy> policy = AbstractGridFormat.OVERVIEW_POLICY.createValue();
         policy.setValue(OverviewPolicy.IGNORE);
 
@@ -130,78 +130,13 @@ class GeoServerExport {
         useJaiRead.setValue(true);
 
         GridCoverage2DReader reader = new GeoTiffReader(file);
-
-        GridEnvelope dimensions = reader.getOriginalGridRange();
-        GridCoordinates maxDimensions = dimensions.getHigh();
-        int w = maxDimensions.getCoordinateValue(0) + 1;
-        int h = maxDimensions.getCoordinateValue(1) + 1;
-        System.out.println("[width, height]: " + w + ", " + h);
 
         GridCoverage2D coverage = reader.read(new GeneralParameterValue[]{policy, gridSize, useJaiRead});
 
         RenderedImage image = coverage.getRenderedImage();
         RandomIter iterator = RandomIterFactory.create(image, null);
-        int value = iterator.getSample(pixels.x, pixels.y, 0);
 
-        System.out.println("total: " + formatter.format(System.nanoTime() - startTime) + " nanoseconds");
-
-        return value;
-    }
-
-    private void readRandomValues(File file, int n) throws IOException, TransformException {
-        long startTime = System.nanoTime();
-        ParameterValue<OverviewPolicy> policy = AbstractGridFormat.OVERVIEW_POLICY.createValue();
-        policy.setValue(OverviewPolicy.IGNORE);
-
-        //this will basically read 4 tiles worth of data at once from the disk...
-        ParameterValue<String> gridSize = AbstractGridFormat.SUGGESTED_TILE_SIZE.createValue();
-
-        //Setting read type: use JAI ImageRead (true) or ImageReaders read methods (false)
-        ParameterValue<Boolean> useJaiRead = AbstractGridFormat.USE_JAI_IMAGEREAD.createValue();
-        useJaiRead.setValue(true);
-
-        GridCoverage2DReader reader = new GeoTiffReader(file);
-        GridEnvelope dimensions = reader.getOriginalGridRange();
-        GridCoordinates maxDimensions = dimensions.getHigh();
-        int w = maxDimensions.getCoordinateValue(0) + 1;
-        int h = maxDimensions.getCoordinateValue(1) + 1;
-        int numBands = reader.getGridCoverageCount();
-
-        GridCoverage2D coverage = reader.read(new GeneralParameterValue[]{policy, gridSize, useJaiRead});
-//        GridGeometry2D geometry = coverage.getGridGeometry();
-
-        Random rnd = new Random();
-        List<Point> points = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int x = rnd.nextInt(w - 1);
-            int y = rnd.nextInt(h - 1);
-            points.add(new Point(x, y));
-        }
-
-//        System.out.println("[width, height]: " + w + ", " + h);
-
-        for (Point point : points) {
-            int x = point.x;
-            int y = point.y;
-//            System.out.println("[x, y]: " + x + ", " + y);
-
-//            Envelope2D pixelEnvelop = geometry.gridToWorld(new GridEnvelope2D(x, y, 1, 1));
-//            double lon = pixelEnvelop.getCenterX();
-//            double lat = pixelEnvelop.getCenterY();
-//            System.out.println("[lon, lat]: " + lon + ", " + lat);
-
-            double[] vals = new double[numBands];
-            coverage.evaluate(new GridCoordinates2D(x, y), vals);
-
-            //Do something!
-//            for (double val : vals) {
-//                System.out.println(val);
-//            }
-        }
-        long time = System.nanoTime() - startTime;
-        System.out.println(formatter.format(n) + " runs.");
-        System.out.println("total: " + formatter.format(time / 1000) + " microseconds");
-        System.out.println("average: " + formatter.format(time / n / 1000) + " microseconds");
+        return iterator.getSample(pixels.x, pixels.y, 0);
     }
 
 }
