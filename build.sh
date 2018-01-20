6cc6fbb46f0193d22b664ec5391d1e12b7cf4769
Lennart Karsten <lennart.k@thinking-aloud.eu>
2018-01-20 16:33:36 +0100

    Update development files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Builds the gis-data JAR and builds the Docker image inside minikube, then restarts the pod.
-
-mvn clean package
-eval $(minikube docker-env)
-docker build -t nexus.informatik.haw-hamburg.de/gis-data-svc .
-kubectl delete pod $(kubectl get pod |grep gis-data |awk '{print $1;}')
--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # Builds the gis-data JAR, builds a docker image, pushes the image to Nexus and restarts the pod.
 
 DOCKER_REGISTRY="nexus.informatik.haw-hamburg.de"
-SERVIE_NAME="gis-data-svc"
+SERVICE_NAME="gis-data-svc"
 
 mvn clean package
 
-docker build -t $DOCKER_REGISTRY/$SERVIE_NAME:dev .
-docker push $DOCKER_REGISTRY/$SERVIE_NAME:dev
+docker build -t ${DOCKER_REGISTRY}/${SERVICE_NAME}:dev -f Dockerfile_dev .
+docker push ${DOCKER_REGISTRY}/${SERVICE_NAME}:dev
 
-kubectl delete pod $(kubectl get pod |grep gis-data |awk '{print $1;}') --force
+kubectl -n mars-mars-beta delete pod --selector=service=gis-data-svc --force
